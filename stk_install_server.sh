1892e999c754cb7cc7c7207aacdf69a1725aaff4
CreamBR-STK <gustavo.barreira92@gmail.com>
2025-10-28 09:22:56 -0300

    Add deps installation dialog

--- stk_install_server.sh
+++ stk_install_server.sh
@@ -6,10 +6,20 @@
 
 TEMP_FILE=$(mktemp)
 
+function root_before_install
+{
+	clear; touch install.log
+	if [ "$EUID" -ne 0 ]; then
+		dialog --title "Install deps" \
+		--passwordbox "Please, enter your root password" 0 0 2> "$TEMP_FILE"
+		echo "$(cat "$TEMP_FILE")" | sudo -S -v
+		rm -f "$TEMP_FILE" 2>/dev/null
+	fi
+}
+
+
 function install_deps
 {
-	dialog 
-
 	dialog --title "Info" \
 		   --title "Server installation" \
 		   --msgbox "You need to install some dependencies to compile SuperTuxKart" 0 0 \
@@ -20,9 +30,54 @@ function install_deps
 		   arch "Arch-based distros (Arch Linux, Manjaro...)" \
 		   suse "openSUSE-based distros (openSUSE, RegataOS...)" 2> "$TEMP_FILE"
 		   
-	OPTION=$(cat "$TEMP_FILE")
+	OPTION=$(cat "$TEMP_FILE")  
 	
 	RETURN_CODE=$?
+	
+		if [ $RETURN_CODE -eq 0 ]; then
+	case "$OPTION" in 
+		deb)    clear; root_before_install
+				dialog --title "Install deps" --textbox install_deps.log 20 65 && \
+				apt-get install build-essential cmake libbluetooth-dev libsdl2-dev \
+				libcurl4-openssl-dev libenet-dev libfreetype6-dev libharfbuzz-dev \
+				libjpeg-dev libogg-dev libopenal-dev libpng-dev \
+				libssl-dev libvorbis-dev libmbedtls-dev pkg-config zlib1g-dev \
+				git subversion sqlite3 libsqlite3-dev
+				;;
+		rpm)    clear; root_before_install
+				dialog --title "Install deps" --textbox install_deps.log 20 65 && \
+				dnf install @development-tools angelscript-devel \
+				bluez-libs-devel cmake desktop-file-utils SDL2-devel \
+				freealut-devel freetype-devel \
+				gcc-c++ git-core libcurl-devel libjpeg-turbo-devel \
+				libpng-devel libsquish-devel libtool libvorbis-devel \
+				openal-soft-devel openssl-devel libcurl-devel harfbuzz-devel \
+				libogg-devel openssl-devel pkgconf \
+				wiiuse-devel zlib-devel \ 
+				sqlite3 sqlite-devel
+				;;
+		arch)   clear; root_before_install
+				dialog --title "Install deps" --textbox install_deps.log 20 65 && \
+				pacman -S openal libogg libvorbis freetype2 harfbuzz curl \
+				bluez-libs openssl libpng zlib libjpeg-turbo sdl2 gcc cmake \
+				pkgconf make git subversion sqlite
+				;;
+		suse)   clear; root_before_install
+				dialog --title "Install deps" --textbox install_deps.log 20 65 && \
+				zypper install gcc-c++ cmake openssl-devel libcurl-devel libSDL2-devel \
+				freetype-devel harfbuzz-devel libogg-devel openal-soft-devel libpng-devel \
+				libvorbis-devel pkgconf zlib-devel enet-devel \
+				libjpeg-devel bluez-devel freetype2-devel \
+				sqlite3 sqlite3-devel
+				;;
+		*)      dialog --textbox "Error! You choose a inexistent option. Try again." 0 0; sleep 1; select_install
+		    ;;
+	esac
+fi
+
+	if [ $RETURN_CODE -eq 1 ]; then
+		clear; select_install
+	fi
 }
 
 function select_install
